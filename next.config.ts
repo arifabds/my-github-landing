411085ee2a0127e122068e253d54b1729c79ffbf
arifabds <arifabdusoglu0016@gmail.com>
2025-07-27 23:19:08 +0300

    rebuild

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
-import { NextConfig } from "next";
-
-const repoName = 'arifabds.github.io';
-const isProd = process.env.NODE_ENV === 'production';
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? `/${repoName}` : '',
-  assetPrefix: isProd ? `/${repoName}/` : '',
 };
-
-export default nextConfig;
+module.exports = nextConfig;
