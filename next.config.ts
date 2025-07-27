3e96984d569064061c72160dbfec817c0361d557
arifabds <arifabdusoglu0016@gmail.com>
2025-07-27 21:53:30 +0300

    build: Configure assetPrefix for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,17 @@
+
 import type { NextConfig } from 'next';
+
+
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   output: 'export',
-
   images: {
     unoptimized: true,
   },
+
+  assetPrefix: isProd ? 'https://arifabds.github.io/' : undefined,
+  basePath: isProd ? '' : undefined, 
 };
 
 export default nextConfig;
